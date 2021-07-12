3d9b6291c4ecc07696d027b13c96e157f824572f
liangzc <1473239369@qq.com>
2021-07-13 05:18:18 +0800

    使用二次开发规范改造

--- Webrule_GetRecordCount/src/index.js
+++ Webrule_GetRecordCount/src/index.js
@@ -1,112 +1,207 @@
 /**
  * 获取数据库表中记录数
  */
-
-	var jsonUtil;
-	var WhereRestrict;
-	var rendererUtil;
-	var queryConditionUtil;
-	var remoteMethodAccessor;
-	var scopeManager;
-	var rpcEnum;
-	var logUtil;
-	exports.initModule = function(sBox) {
-		jsonUtil = sBox.getService("vjs.framework.extension.util.JsonUtil");
-		WhereRestrict = sBox.getService("vjs.framework.extension.platform.services.where.restrict.WhereRestrict");
-		remoteMethodAccessor = sBox.getService("vjs.framework.extension.platform.services.operation.remote.RemoteMethodAccessor");
-		rendererUtil = sBox.getService("vjs.framework.extension.platform.services.view.widget.common.error.ErrorUtil");
-		queryConditionUtil = sBox.getService("vjs.framework.extension.platform.services.where.restrict.QueryCondUtil");
-		scopeManager = sBox.getService("vjs.framework.extension.platform.interface.scope.ScopeManager");
-		logUtil = sBox.getService("vjs.framework.extension.util.log");
-	}
-
-	var main = function(ruleContext) {
-		var ruleCfg = ruleContext.getRuleCfg();
-		var params = ruleCfg["inParams"];
-		var ruleInstId = ruleCfg["ruleInstId"];
-		var inParamsObj = jsonUtil.json2obj(params);
-		var dsWhere = inParamsObj["dsWhere"]; // 获取条件
-		var queryparam = inParamsObj["queryparam"];
-		var routeContext = ruleContext.getRouteContext();
-		var sqlStr = "";
-		var wrParam = {
-				"fetchMode": "custom",
-				"routeContext": routeContext
+vds.import("vds.ds.*", "vds.exception.*", "vds.log.*", "vds.rpc.*", "vds.string.*", "vds.component.*", "vds.window.*", "vds.widget.*", "vds.expression.*")
+/**
+ * 规则入口
+ */
+var main = function (ruleContext) {
+	return new Promise(function (resolve, reject) {
+		try {
+			var inParamsObj = ruleContext.getVplatformInput();
+			if (!inParamsObj) {//建议兼容
+				inParamsObj = "";
+			}
+			var dsWhere = inParamsObj["dsWhere"]; // 获取条件
+			var queryparam = inParamsObj["queryparam"];
+			var sqlStr = "";
+			var wrParam = {
+				"type": vds.ds.WhereType.Query,
+				"methodContext": ruleContext.getMethodContext()
 			};
-		var w = WhereRestrict.init(wrParam);
-		var scope = scopeManager.getScope();
-
-		if (undefined != queryparam && null != queryparam) {
-			var params = queryConditionUtil.genCustomParams({
-				"paramDefines": queryparam,
-				"routeContext": routeContext
-			});
-			w.addExtraParameters(params);
-		}
-		if (undefined != dsWhere && null != dsWhere && dsWhere.length > 0) {
-			// 有进行字段的配置，按照配置生成条件sql
-			w.andExtraCondition(dsWhere, "custom");
-		}
-		inParamsObj["sql"] = sqlStr + w.toWhere();
-		inParamsObj["parameters"] = w.toParameters();
-
-		var resultCount = 0;
-
-		var inputParams = {
-			// ruleSetCode为活动集编号
-			"ruleSetCode": "CommonRule_GetRecordCount",
-			// params为活动集输入参数
-			"params": {
-				"InParams": jsonUtil.obj2json(inParamsObj)
+			var w = vds.ds.createWhere(wrParam);
+			if (undefined != queryparam && null != queryparam) {
+				var params = genCustomParams(queryparam, ruleContext);
+				w.addExtraParameters(params);
 			}
-		};
-		var callback = function(responseObj) {
-			var resultCount = 0;
-			var success = responseObj.IsSuccess;
-			if (!success) {
-				rendererUtil.handleError("获取数据库记录数失败");
-				return false;
-			} else {
-				resultCount = responseObj.RecordCount;
+			if (undefined != dsWhere && null != dsWhere && dsWhere.length > 0) {
+				// 有进行字段的配置，按照配置生成条件sql
+				w.addCondition(dsWhere);
 			}
-			if (ruleContext.setBusinessRuleResult) {
-				ruleContext.setBusinessRuleResult({
-					recordCount: resultCount
-				});
-			}
-			ruleContext.fireRouteCallback();
-			return true;
-		};
+			inParamsObj["sql"] = sqlStr + w.toWhere();
+			inParamsObj["parameters"] = w.toParameters();
+			var callback = function (responseObj) {
+				var resultCount = 0;
+				var success = responseObj.IsSuccess;
+				if (!success) {
+					reject(vds.exception.newSystemException("获取数据库记录数失败"));
+					return false;
+				} else {
+					resultCount = responseObj.RecordCount;
+				}
+				if (ruleContext.setResult) {
+					ruleContext.setResult("recordCount", resultCount);
+				}
+				resolve();
+				return true;
+			};
 
-		var errorCallback = function(responseObj) {
-			logUtil.error(responseObj.message);
-			ruleContext.handleException(responseObj);
-		};
-		//  调用后台活动集
-		var sConfig = {
-			"isAsyn": true,
-			"componentCode": scope.getComponentCode(),
-			"transactionId": routeContext.getTransactionId(),
-			ruleSetCode: "CommonRule_GetRecordCount",
-			commitParams: [{
+			var errorCallback = function (responseObj) {
+				vds.log.error(responseObj.message);
+				reject(responseObj);
+			};
+			//  调用后台活动集
+			var promise = vds.rpc.callCommand("CommonRule_GetRecordCount", [{
 				"paramName": "InParams",
 				"paramType": "char",
-				"paramValue": inputParams.params.InParams
-			}],
-			error: errorCallback,
-			afterResponse: callback
+				"paramValue": vds.string.toJson(inParamsObj)
+			}], {
+				"isAsync": true
+			});
+			promise.then(callback).catch(errorCallback);
+		} catch (err) {
+			reject(err);
 		}
-		var scopeId = scope.getInstanceId();
-		var windowScope = scopeManager.getWindowScope();
-		if (scopeManager.isWindowScope(scopeId)) {
-			sConfig.windowCode = windowScope.getWindowCode();
+	});
+}
+var genCustomParams = function (paramDefines, ruleContext) {
+	var rs = {};
+	if (paramDefines && paramDefines.length > 0) {
+		for (var i = 0; i < paramDefines.length; i++) {
+			var define = paramDefines[i];
+			var key = define["queryfield"];
+			if (!key) {
+				key = define["Queryfield"];
+			}
+			var valueDefine = define["queryfieldValue"];
+			if (!valueDefine) {
+				valueDefine = define["QueryfieldValue"];
+			}
+			var type = define["type"];
+			var componentControlID = define["componentControlID"]
+			var value = getCustomParamValue(valueDefine, type, componentControlID, ruleContext);
+			rs[key] = value;
 		}
-		
-		remoteMethodAccessor.invoke(sConfig);
-		ruleContext.markRouteExecuteUnAuto();
-		return true;
 	}
+	return rs;
+}
+/**
+ * 获取自定义参数的值
+ * @param queryfieldValue 参数值
+ * @param type 参数类源类型(参数值类型1表字段，2系统变量，3组件变量，4固定值，5自定义，6面板参数，8控件的值, 9表达式)
+ * @param componentControlId 参数来源控件
+ */
+var getCustomParamValue = function (queryfieldValue, type, componentControlId, ruleContext) {
+	var returnValue = "";
 
-	exports.main = main;
+	switch (vds.string.trim(type + "")) {
+		case "1":
+			if (queryfieldValue.indexOf(".") == -1) {
+				vds.log.warn(queryfieldValue + " 格式必须为表名.字段名");
+				break;
+			}
+			var ds = queryfieldValue.split(".")[0];
+			var fieldName = queryfieldValue.split(".")[1];
+			var record = getCurrentRecord(ds);
+			returnValue = record.get(fieldName);
+			break;
+		case "2":
+			returnValue = vds.component.getVariant(queryfieldValue);
+			break;
+		case "3":
+			returnValue = vds.window.getInput(queryfieldValue);
+			break;
+		case "4":
+			// returnValue = queryfieldValue;
+			// 固定值(0:假，1:真，2:空)
+			switch ((queryfieldValue + "").toLowerCase()) {
+				case "0":
+					returnValue = false;
+					break;
+				case "1":
+					returnValue = true;
+					break;
+				case "2":
+					returnValue = null;
+					break;
+				default:
+					returnValue = queryfieldValue;
+					break;
+			}
+			break;
+		case "5":
+			returnValue = queryfieldValue;
+			break;
+		case "6":
+			var valueQueryControlID = componentControlId;
+			var value = queryfieldValue;
+			var storeType = vds.widget.getStoreType(valueQueryControlID);
+			var storeTypes = vds.widget.StoreType;
+			// 按照控件不同的属性类型，获取参数值
+			var ds = getDsName(valueQueryControlID);
+			var record = getCurrentRecord(ds);
+			if (storeTypes.Set == storeType) {
+				// 集合类控件，组装表名.字段名进行取值
+				if (record) {
+					var field = value.split("_")[1];
+					returnValue = record.get(field);
+				} else {
+					vds.log.error("集合控件:" + valueQueryControlID + " 无选中行，无法获取其参数值");
+				}
+			} else if (storeTypes.SingleRecordMultiValue == storeType) {
+				// 单记录多值控件，按照控件属性名字取得关联的标识，再进行取值
+				//var propertyCode = value.split("_")[1];
+				var propertyCode = "";
+				// 目前认为使用-分隔，也可以使用_分隔
+				if (value.indexOf("-") != -1) {
+					propertyCode = value.split("-")[1];
+				} else {
+					propertyCode = value.split("_")[1];
+				}
+				var fieldCode = vds.widget.getProperty(valueQueryControlID, propertyCode);
+				returnValue = record.get(fieldCode);
+			} else if (storeTypes.SingleRecord == storeType) {
+				// 单值控件，直接取值
+				var fieldCode = vds.widget.getFieldCodes(ds, valueQueryControlID)[0];
+				returnValue = record.get(fieldCode);
+				if (null == returnValue || undefined == returnValue) {
+					returnValue = "";
+				}
+			}
+			break;
+		case "8":
+		case "9":
+		default:
+			if (!queryfieldValue) {// modify by xiedh 2016-04-26,预先校验，防止执行表达式报错
+				if (null == queryfieldValue || undefined == queryfieldValue) {
+					returnValue = null;
+				} else {
+					returnValue = queryfieldValue;
+				}//end modify
+			} else {
+				returnValue = vds.expression.execute(queryfieldValue, {
+					"ruleContext": ruleContext
+				});
+			}
+			break;
+	}
+	//todo
+	if (queryfieldValue !== "\"\"" && returnValue === "") {
+		return null;
+	}
+	// 统一输出为字符串
+	//return (null == returnValue || undefined == returnValue ? "" : returnValue);
+	return (undefined == returnValue ? null : returnValue);
+}
+var getCurrentRecord = function (ds) {
+	var datasource = vds.ds.lookup(ds);
+	return datasource.getCurrentRecord();
+}
 
-export{    main}
+var getDsName = function (widgetCode) {
+	var dsNames = vds.widget.getDatasourceCodes(widgetCode);
+	return dsNames[0];
+}
+export {
+	main
+}
--- Webrule_I18nOperation/src/index.js
+++ Webrule_I18nOperation/src/index.js
@@ -1,318 +1,272 @@
 /**
  * 多语言操作
  */
-
-
-	var jsonUtil, scopeManager, remoteServer;
-	var sandbox;
-	var i18n;
-	//cookie里i18n标识
-	var VPLATFORMI18NIDEN = "langCookie";
-	var frontAlert,logUtil,ProgressBarUtil,ExpressionContext,formulaUtil;
-
-	exports.initModule = function(sb) {
-		sandbox = sb;
-		jsonUtil = sb.getService("vjs.framework.extension.util.JsonUtil");
-		remoteServer = sb.getService("vjs.framework.extension.platform.services.operation.remote.RemoteMethodAccessor");
-		scopeManager = sb.getService("vjs.framework.extension.platform.interface.scope.ScopeManager");
-		widgetProperty = sb.getService("vjs.framework.extension.platform.services.view.widget.common.action.WidgetProperty");
-		windowParam = sb.getService("vjs.framework.extension.platform.services.param.manager.WindowParam");
-		componentParam = sb.getService("vjs.framework.extension.platform.services.param.manager.ComponentParam");
-		dbService = sb.getService("vjs.framework.extension.platform.services.view.logic.datasource.DatasourceUtil");
-		dsFactory = sb.getService("vjs.framework.extension.platform.interface.model.datasource.DatasourceFactory");
-		i18n = sb.getService("vjs.framework.extension.platform.interface.i18n.platform");
-		frontAlert = sb.getService("vjs.framework.extension.platform.interface.alerter.FrontEndAlerter");
-		logUtil = sb.getService("vjs.framework.extension.util.log");
-		formulaUtil = sb.getService("vjs.framework.extension.platform.engine.expression.ExpressionEngine");
-		ProgressBarUtil = sb.getService("vjs.framework.extension.platform.services.view.widget.common.progressbar.ProgressBarUtil");
-		ExpressionContext = sb.getService("vjs.framework.extension.platform.engine.expression.ExpressionContext");
-	}
-
-	/**
-	 * 调用command
-	 * @param	{Object}	params	提交的参数
-	 * @param	{Function}	success	成功的回调
-	 * @param	{Function}	error	失败的回调
-	 * */
-	var InvokeCommand = function(params,success,error){
-		var scope = scopeManager.getWindowScope();
-		var sConfig = {
-				"isAsyn": false,
-				"componentCode": scope.getComponentCode(),
-				"windowCode": scope.getWindowCode(),
-				ruleSetCode: "I18nOperation",
-				isRuleSetCode:false,
-				commitParams: [params],
-				error:error,
-				afterResponse: success
-		}
-		remoteServer.invoke(sConfig);
-	}
-	
-	/**
-	 * 获取语言值
-	 * */
-	function getLanguage(){
-		var arr,reg=new RegExp("(^| )"+VPLATFORMI18NIDEN+"=([^;]*)(;|$)");
-		if(arr=document.cookie.match(reg))
-			return unescape(arr[2]);
-		else
-			return null;
-	}
-	
-	/**
-	 * 把当前语言设置到localStorage
-	 * */
-	function setLanguage(value){
-//		document.cookie = VPLATFORMI18NIDEN + "=" + value;
-		localStorage.setItem(VPLATFORMI18NIDEN,value);
-		VMetrix.putAllVjsContext({
-			language:value
-		},1);
-		window.location.reload();
-	}
-	
-	var main = function(ruleContext) {
-		var ruleCfgValue = ruleContext.getRuleCfg(),
-			inParams = ruleCfgValue["inParams"];
-
-		if (inParams && "" != inParams) {
-			var inParamObj = jsonUtil.json2obj(inParams),
-				languageOperation = inParamObj.languageOperation,
-				lang = inParamObj.language,
-				languageType = inParamObj.languageType,
-				returnMapping = inParamObj.returnMapping;
-
-			var scope = scopeManager.getWindowScope();
-			var componentCode = scope.getComponentCode();
-			var windowScope = scope.getWindowCode();
-
-			switch(languageOperation){
-				case "getCurLanguage":
-					var value = localStorage.getItem(VPLATFORMI18NIDEN);
-					_setDataToObject(returnMapping, value, ruleContext);
-//					var arr,reg=new RegExp("(^| )"+VPLATFORMI18NIDEN+"=([^;]*)(;|$)");
-//					if(arr = document.cookie.match(reg)){
-//						value = unescape(arr[2]);
-//					}else{
-//						logUtil.info("当前cookie没有保存语言.");
-//					}
-					break;
-				case "getLanguages":
-					var params = {};
-					params["paramName"] = "InParams";
-					params["paramType"] = "object";
-					params["paramValue"] = {
-						operation : "getAll",
-					};
-					var success = scopeManager.createScopeHandler({
-						handler : function(returnJson){
-							if(returnJson && returnJson.data && returnJson.data.types){
-								var types = returnJson.data.types;
-								_setDataToObject(returnMapping, types, ruleContext)
-							}
-							ProgressBarUtil.hideProgress(true);
-							ruleContext.fireRouteCallback();
-						}
-					});
-					var error = function(returnJson){
-						ProgressBarUtil.hideProgress(true);
-						frontAlert.error({
-							title:i18n.get("后台处理异常","获取全部语种时，后台报错的弹框标题"),
-							msgHeader: i18n.get("获取多语言信息异常","获取全部语种时，后台报错的错误信息标题"),
-							msg:i18n.get("无法获取语种列表. 错误信息：","获取全部语种时，后台返回的错误信息") + (returnJson.msg ? returnJson.msg : ""),
-							detail:i18n.get("暂无","获取全部语种时，后台返回的详细错误信息"),
-							callback:function(){
-								ruleContext.fireRouteCallback();
-							}
-						});
-					}
-					ProgressBarUtil.showProgress(i18n.get("正在获取语言列表","获取语言列表时进度条的提示文字"), true);
-					InvokeCommand(params,success,error);
-					ruleContext.markRouteExecuteUnAuto();
-					break;
-				case "setCurLanguage":
-					var context = new ExpressionContext();
-					context.setRouteContext(ruleContext.getRouteContext());
-					var value = formulaUtil.execute({
-						"expression": inParamObj.language,
-						"context": context
-					});
-					if(null != value && "" != value){
-						setLanguage(value);
-					}else{
-						frontAlert.error({
-							title:i18n.get("配置异常","无法设置语言的弹框标题"),
-							msgHeader: i18n.get("设置语言异常","无法设置语言的错误信息标题"),
-							msg:i18n.get("无法设置语言. 错误信息：语言编码为空","无法设置语言的错误信息")
-						});
-					}
-					break;
-			}
-		}
-	}
-	/*执行表达式*/
-    var experssFunc = function(experss, routeContext) {
-        var context = new ExpressionContext();
-        context.setRouteContext(routeContext);
-        if (undefined == experss || null == experss) return null;
-        var resultValue = engine.execute({
-            "expression": experss,
-            "context": context
-        });
-        return resultValue;
-    }
-
-	// 创建游离 DB 对象信息
-	var _createDBInfo = function(types) {
-		var len = 0;
-		var objs = [];
-		var len = types.length;
-		for(var i = 0; i < len; i++){
-			var type = types[i];
-			var map = {};
-			map.id = i;
-			map.code = type.code;
-			map.name = type.name;
-			map.icon = type.icon;
-			objs.push(map);
-		}
-		var result = new Object,
-			metadata = new Object,
-			model = new Object;
-
-		var datas = {
-			"recordCount": len,
-			"values": objs
-		};
-		result.datas = datas;
-
-		var fields = [{
-			"code": "id",
-			"name": "id",
-			"length": 255,
-			"type": "char",
-			"defaultValue": "",
-			"precision": ""
-		}, {
-			"code": "code",
-			"name": "code",
-			"length": 255,
-			"type": "char",
-			"defaultValue": "",
-			"precision": ""
-		}, {
-			"code": "name",
-			"name": "name",
-			"length": 255,
-			"type": "char",
-			"defaultValue": "",
-			"precision": ""
-		},{
-			"code": "icon",
-			"name": "icon",
-			"length": 255,
-			"type": "char",
-			"defaultValue": "",
-			"precision": ""
-		}];
-
-		model.datasourceName = "FreeLangListTb";
-		model.fields = fields;
-		metadata.model = [model];
-		result.metadata = metadata;
-
-		return result;
-	};
-
-	var _setDataToObject = function(returnMapping, value, ruleContext) {
-		if (returnMapping && returnMapping.length > 0) {
-			for (var i = 0; i < returnMapping.length; i++) {
-				var mapping = returnMapping[i],
-					dest = mapping["dest"];
-
-				var destType = mapping["destType"], //目标类型（entity：实体，control：控件，windowVariant：窗体变量，systemVariant：系统变量）
-					src = mapping["src"], //来源(returnValue:返回值，expression:表达式)
-					srcType = mapping["srcType"]; //来源(当目标类型是实体时，返回实体存在此处)
-
-				// 目标对象为实体
-				if (dbService.isEntity(dest, destType, ruleContext)) {
-					var freeDbInfo = _createDBInfo(value),
-						freeDb = dsFactory.unSerialize(freeDbInfo),
-						srcRecords = freeDb.getAllRecords().toArray(),
-						destFieldMapping = mapping["destFieldMapping"],
-						updateDestEntityMethod = mapping["updateDestEntityMethod"],
-						isCleanDestEntityData = mapping["isCleanDestEntityData"];
-
-					if (updateDestEntityMethod == null)
-						updateDestEntityMethod = "insertOrUpdateBySameId";
-					dbService.insertOrUpdateRecords2Entity(dest, destType, srcRecords, destFieldMapping, updateDestEntityMethod, isCleanDestEntityData, ruleContext);
-				} else {
-					var routeContext = ruleContext.getRouteContext();
-					switch (destType) {
-						case "control":
-							if (dest.indexOf(".") == -1) {
-								// 目标不存在.表示为单值控件
-//								widgetDatasource.setSingleValue(target, srcVal);
-								//兼容一些不是没有绑定数据库的控件，如：检索控件
-								widgetProperty.set(dest,"Value",value);
-							} else {
-								// 目标存在.表示为多值控件
-								var widgetId = dest.split(".")[0];
-								var propertyCode = dest.split(".")[1];
-								widgetProperty.set(widgetId, propertyCode, value);
-							}
-							break;
-						case "windowVariant":
-							windowParam.setInput({
-								"code": dest,
-								"value": value
-							});
-							break;
-						case "systemVariant":
-							componentParam.setVariant({
-								"code": dest,
-								"value": value
-							});
-							break;
-						case "ruleSetVariant":
-							routeContext.setVariable(dest, value);
-							break;
-						case "ruleSetOutput":
-							routeContext.setOutputParam(dest, value);
-							break;
-						case "windowOutput":
-							// 给当前窗体输出变量赋值
-							windowParam.setOutput({
-								"code": dest,
-								"value": value
-							});
-							break;
-						default:
-							log.error("无效的目标类型：" + destType);
-							break;
-					}
-				}
-			}
-		}
-	};
-
-	/**
-	 * 给控件赋值
-	 */
-    var _setWidgetValue=function(destWidgetId,value){
-    	if(destWidgetId!=null && destWidgetId.indexOf(".") != -1){
-               var splits = destWidgetId.split(".");
-               var widgetId = splits[0];
-               var dbFieldName = vmMappingUtil.getRefFieldFromWidgetPropertyCode(destWidgetId);
-               var valueObj = {};
-               valueObj[dbFieldName] = value;
-               viewModel.getDataModule().setSingleRecordMultiValue(widgetId, valueObj);
-       }else{
-              viewModel.getDataModule().setSingleValue(destWidgetId, value);
-       }
-    };
-
-	exports.main = main;
-
-export{    main}
+/**
+ *	
+ */
+ vds.import("vds.progress.*", "vds.message.*", "vds.i18n.*", "vds.expression.*", "vds.rpc.*", "vds.ds.*", "vds.widget.*", "vds.window.*", "vds.component.*", "vds.log.*", "vds.exception.*")
+ /**
+  * 规则入口
+  */
+ var main = function (ruleContext) {
+	 return new Promise(function (resolve, reject) {
+		 try {
+			 var inParamObj = ruleContext.getVplatformInput();
+			 if (!inParamObj) { //建议兼容
+				 inParamObj = "";
+			 }
+			 var languageOperation = inParamObj.languageOperation,
+				 returnMapping = inParamObj.returnMapping;
+			 switch (languageOperation) {
+				 case "getCurLanguage":
+					 var value = localStorage.getItem(VPLATFORMI18NIDEN);
+					 _setDataToObject(returnMapping, value, ruleContext, false);
+					 resolve();
+					 break;
+				 case "getLanguages":
+					 var params = {};
+					 params["code"] = "InParams";
+					 params["type"] = "object";
+					 params["value"] = {
+						 operation: "getAll",
+					 };
+					 var success = function (returnJson) {
+						 if (returnJson && returnJson.data && returnJson.data.types) {
+							 var types = returnJson.data.types;
+							 _setDataToObject(returnMapping, types, ruleContext, true)
+						 }
+						 vds.progress.hide(true);
+						 resolve();
+					 }
+					 success = ruleContext.genAsynCallback(success);
+					 var error = function (returnJson) {
+						 vds.progress.hide(true);
+						 var promise = vds.message.error("无法获取语种列表." + (returnJson.msg ? " 错误信息：" + returnJson.msg : ""));
+						 promise.then(resolve).catch(reject);
+					 }
+					 vds.progress.show(vds.i18n.get("正在获取语言列表", "获取语言列表时进度条的提示文字"), true);
+					 InvokeCommand(params, success, error);
+					 break;
+				 case "setCurLanguage":
+					 var value = vds.expression.execute(inParamObj.language, {
+						 "ruleContext": ruleContext
+					 });
+					 if (null != value && "" != value) {
+						 setLanguage(value);
+					 } else {
+						 var promise = vds.message.error("无法设置语言. 错误信息：语言编码为空");
+						 promise.then(resolve).catch(reject);
+					 }
+					 break;
+			 }
+		 } catch (err) {
+			 reject(err);
+		 }
+	 });
+ }
+ //cookie里i18n标识
+ var VPLATFORMI18NIDEN = "langCookie";
+ 
+ /**
+  * 调用command
+  * @param	{Object}	params	提交的参数
+  * @param	{Function}	success	成功的回调
+  * @param	{Function}	error	失败的回调
+  * */
+ var InvokeCommand = function (params, success, error) {
+	 var promise = vds.rpc.callCommand("I18nOperation", [params], {
+		 "isRuleSetCode": false
+	 });
+	 promise.then(success).catch(error);
+ }
+ 
+ /**
+  * 把当前语言设置到localStorage
+  * */
+ function setLanguage(value) {
+	 //		document.cookie = VPLATFORMI18NIDEN + "=" + value;
+	 localStorage.setItem(VPLATFORMI18NIDEN, value);
+	 // VMetrix.putAllVjsContext({//
+	 // 	language: value
+	 // }, 1);
+	 window.location.reload();
+ }
+ 
+ 
+ // 创建游离 DB 对象信息
+ var _createDBInfo = function (types) {
+	 var len = 0;
+	 var objs = [];
+	 var len = types.length;
+	 for (var i = 0; i < len; i++) {
+		 var type = types[i];
+		 var map = {};
+		 map.id = i;
+		 map.code = type.code;
+		 map.name = type.name;
+		 map.icon = type.icon;
+		 objs.push(map);
+	 }
+ 
+	 var fields = [{
+		 "code": "id",
+		 "name": "id",
+		 "length": 255,
+		 "type": "char",
+		 "defaultValue": "",
+		 "precision": ""
+	 }, {
+		 "code": "code",
+		 "name": "code",
+		 "length": 255,
+		 "type": "char",
+		 "defaultValue": "",
+		 "precision": ""
+	 }, {
+		 "code": "name",
+		 "name": "name",
+		 "length": 255,
+		 "type": "char",
+		 "defaultValue": "",
+		 "precision": ""
+	 }, {
+		 "code": "icon",
+		 "name": "icon",
+		 "length": 255,
+		 "type": "char",
+		 "defaultValue": "",
+		 "precision": ""
+	 }];
+	 var result = vds.ds.unSerialize(fields, {
+		 "dsCode": "FreeLangListTb",
+		 "datas": objs
+	 });
+	 return result;
+	 /**
+	  * 
+	  * @param {Array<Object>} returnMapping
+	  * @return {*}
+	  */
+ };
+ var _setDataToObject = function (returnMapping, value, ruleContext, isEntity) {
+	 if (returnMapping && returnMapping.length > 0) {
+		 for (var i = 0; i < returnMapping.length; i++) {
+			 var mapping = returnMapping[i],
+				 dest = mapping["dest"];
+ 
+			 var destType = mapping["destType"]; //目标类型（entity：实体，control：控件，windowVariant：窗体变量，systemVariant：系统变量）
+			 var methodContext = ruleContext.getMethodContext();
+			 // 目标对象为实体
+			 if (isEntity) {
+				 var freeDb = _createDBInfo(value),
+					 srcRecords = freeDb.getAllRecords().toArray(),
+					 destFieldMapping = mapping["destFieldMapping"],
+					 updateDestEntityMethod = mapping["updateDestEntityMethod"],
+					 isCleanDestEntityData = mapping["isCleanDestEntityData"];
+				 var destEntity = getDatasource(dest, destType);
+				 var newMappings = [];
+				 for (var j = 0, len = destFieldMapping.length; j < len; j++) {
+					 newMappings.push({
+						 "code": destFieldMapping[j]["destField"],
+						 "type": destFieldMapping[j]["srcValueType"],
+						 "value": destFieldMapping[j]["srcValue"]
+					 })
+				 }
+				 var isClear = false;
+				 var mergeType;
+				 switch (updateDestEntityMethod) {
+					 case "loadRecord":
+						 mergeType = vds.ds.MergeType.Load;
+						 isClear = true;
+						 break;
+					 case "updateRecord":
+						 mergeType = vds.ds.MergeType.Update;
+						 break;
+					 default:
+						 mergeType = vds.ds.MergeType.InsertOrUpdate;
+						 isClear = isCleanDestEntityData;
+						 break;
+				 }
+				 vds.ds.merge(destEntity, srcRecords, newMappings, mergeType, methodContext, {
+					 "isClear": isClear
+				 });
+			 } else {
+				 switch (destType) {
+					 case "control":
+						 if (dest.indexOf(".") == -1) {
+							 // 目标不存在.表示为单值控件
+							 //兼容一些不是没有绑定数据库的控件，如：检索控件
+							 vds.widget.execute(dest, "setValue", [value]);
+						 } else {
+							 // 目标存在.表示为多值控件
+							 var widgetId = dest.split(".")[0];
+							 var propertyCode = dest.split(".")[1];
+							 vds.widget.execute(widgetId, propertyCode, [value]);
+						 }
+						 break;
+					 case "windowVariant":
+						 vds.window.setInput(dest, value);
+						 break;
+					 case "systemVariant":
+						 vds.component.setVariant(dest, value);
+						 break;
+					 case "ruleSetVariant":
+						 methodContext.setVariable(dest, value);
+						 break;
+					 case "ruleSetOutput":
+						 methodContext.setOutput(dest, value);
+						 break;
+					 case "windowOutput":
+						 // 给当前窗体输出变量赋值
+						 vds.window.setOutput(dest, value);
+						 break;
+					 default:
+						 vds.log.error("无效的目标类型：" + destType);
+						 break;
+				 }
+			 }
+		 }
+	 }
+ };
+ /**
+  * 获取数据源
+  * @param {String} dsCode 数据源编码
+  * @param {String} type 数据源类型
+  * @param {@link MethodContext} methodContext 方法上下文
+  * @return {@link Datasource} 数据源实例
+  */
+ var getDatasource = function (dsCode, type, methodContext) {
+	 var err_msg = "";
+	 var datasource;
+	 switch (type) {
+		 case "windowVariant": //窗体输入
+		 case "windowInput":
+			 datasource = vds.window.getInput(dsCode);
+			 err_msg = "窗体输入";
+			 break;
+		 case "ruleSetVariant": //方法变量
+			 datasource = methodContext.getVariable(dsCode);
+			 err_msg = "方法变量";
+			 break;
+		 case "ruleSetOutput": //方法输出
+			 datasource = methodContext.getOutput(dsCode);
+			 err_msg = "方法输出";
+			 break;
+		 case "windowOutput": //窗体输出
+			 datasource = vds.window.getOutput(dsCode);
+			 err_msg = "窗体输出";
+			 break;
+		 default: //界面实体
+			 datasource = vds.ds.lookup(dsCode);
+			 err_msg = "";
+			 break;
+	 }
+	 if (!datasource) {
+		 throw vds.exception.newConfigException(err_msg + "实体【" + dsCode + "】不存在，请检查配置.");
+	 }
+	 return datasource;
+ }
+ 
+ export {
+	 main
+ }
